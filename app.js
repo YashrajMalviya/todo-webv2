301a1a3111de61193d8d7374c62c489de7ab2064
YashrajMalviya <yashrajmalviya296@gmail.com>
2024-03-17 01:45:21 +0530

    data inserted in Mongo

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-//jshint esversion:6
-
+//---------SETUP CODE----------//
+//importing essential modules
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
+//using express application
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -11,14 +12,42 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
+//---------------------LOGICAL--------------------------//
+
+//MongoDB logic
+//connecting/creating to a new database in monogodb on port27027
+mongoose.connect("mongodb://localhost:27017/todolistDB",{useNewUrlParser:true});
+//creating schema based on the data to store
+const itemsSchema = {
+  name: String
+};
+//creting model to get the data of schema type mentioned
+const Item = mongoose.model("Item",itemsSchema);
+
+//creting documents in collection Items(table)
+const item1 = new Item({
+  name: "Welcome to your todoList!"
+});
+const item2 = new Item({
+  name: "Hit the + button to add a new item"
+});
+const item3 = new Item({
+  name: "<---- hit this to delete an item"
+});
+
+const defaultItems = [item1,item2,item3];           //array
+
+//Inserting into Items collection in database
+mongoose.connection.once('open', function() {
+  console.log('Connected to MongoDB');
+
+  // Insert default items once connected
+  Item.insertMany(defaultItems);
+});
 
 app.get("/", function(req, res) {
 
-const day = date.getDate();
-
-  res.render("list", {listTitle: day, newListItems: items});
+  res.render("list", {listTitle: "Today", newListItems: items});
 
 });
 
